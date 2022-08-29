2c465f61be84a3c70b8433743deccd91fa16455c
Sophia Aramburu <sophiaconstance12@gmail.com>
2022-08-28 18:01:28 -0800

    fixing some issues

--- script.js
+++ script.js
@@ -1,2 +1,44 @@
-var currentDay = moment("8-28-2022", "M-D-YYYY").format("MM-DD-YYYY");
-$('#currentDay').text(currentDay);
+$(document).ready(function() {
+    $("#moment").text (moment().format("MMMM Do YY, h:mm:ss"));
+    $(".saveBtn").on("click",function() {
+console.log(this);
+
+var text = $(this).siblings(".description").val();
+var time = $(this).parent().attr("id");
+localStorage.setItem(time ,text);
+    })
+$('#hour1 .description').val(localStorage.getItem('hour1')) ;
+$('#hour2 .description').val(localStorage.getItem('hour2')) ;
+$('#hour3 .description').val(localStorage.getItem('hour3')) ;
+$('#hour4 .description').val(localStorage.getItem('hour4')) ;
+$('#hour5 .description').val(localStorage.getItem('hour5')) ;
+$('#hour6 .description').val(localStorage.getItem('hour6')) ;
+$('#hour7 .description').val(localStorage.getItem('hour7')) ;
+$('#hour8 .description').val(localStorage.getItem('hour8')) ;
+$('#hour9 .description').val(localStorage.getItem('hour9')) ;
+$('#hour10 .description').val(localStorage.getItem('hour10')) ;
+function currentTime(){
+    var later = moment().hour();
+    $('.time-block').each(function () {
+        var timeBlock=parseint($(this).attr('id').split('hour')[1])
+        console.log(later, timeBlock)
+        if (later > timeBlock){
+            $(this).addClass('past');
+            $(this).removeClass('future');
+            $(this).removeClass('present');
+        }
+        else if (later === timeBlock){
+            $(this).removeClass('past');
+            $(this).addClass('present');
+            $(this).removeClass('future');
+        }
+        else {
+            $(this).removeClass('present');
+            $(this).removeClass('past');
+            $(this).addClass('future');
+        }
+    }
+    )
+}
+currentTime();
+})
