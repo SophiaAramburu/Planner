7d8c7b1ec3c46536cc52e0b723c0f7b344fa2002
Sophia Aramburu <sophiaconstance12@gmail.com>
2022-08-30 16:28:16 -0800

    fourth commit

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
-    $("#moment").text (moment().format("MMMM Do YY, h:mm:ss"));
+    $("#moment").text(moment().format("MMMM Do YY, h:mm:ss"));
     $(".saveBtn").on("click",function() {
 console.log(this);
 
 var text = $(this).siblings(".description").val();
 var time = $(this).parent().attr("id");
 localStorage.setItem(time ,text);
+console.log(time)
     })
 $('#hour1 .description').val(localStorage.getItem('hour1')) ;
 $('#hour2 .description').val(localStorage.getItem('hour2')) ;
@@ -18,23 +19,19 @@ $('#hour8 .description').val(localStorage.getItem('hour8')) ;
 $('#hour9 .description').val(localStorage.getItem('hour9')) ;
 $('#hour10 .description').val(localStorage.getItem('hour10')) ;
 function currentTime(){
-    var later = moment().hour();
-    $('.time-block').each(function () {
-        var timeBlock=parseint($(this).attr('id').split('hour')[1])
-        console.log(later, timeBlock)
-        if (later > timeBlock){
+    var later = moment().hour()-7;
+    console.log(later);
+    $('.description').each(function () {
+        var timeBlock= $(this).parent().attr('id').split('hour')[1];
+        console.log(timeBlock);
+        console.log(later, timeBlock);
+        if (later < timeBlock){
             $(this).addClass('past');
-            $(this).removeClass('future');
-            $(this).removeClass('present');
         }
-        else if (later === timeBlock){
-            $(this).removeClass('past');
+        else if (later == timeBlock){
             $(this).addClass('present');
-            $(this).removeClass('future');
         }
         else {
-            $(this).removeClass('present');
-            $(this).removeClass('past');
             $(this).addClass('future');
         }
     }
